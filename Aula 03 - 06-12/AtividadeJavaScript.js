27db3e1afc3d564250b416dd6b891013e38383fb
LarissaNeitzke <larissasouza.neitzke@gmail.com>
2024-12-06 21:13:48 -0300

    Add files via upload

---
+++ Aula 03 - 06-12/AtividadeJavaScript.js
@@ -0,0 +1,64 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+console.log('Hello World');
+
+/*AND &&
+let A = true
+let B = false
+let resultado = A && B
+console.log(A, '&&', B, '=', resultado)*/
+
+/*NOT !=
+let A = true
+let B = false
+let resultado = A != B
+console.log(A, '!=', B, '=', resultado)*/
+
+/*OR || - SE UM FOR TRUE VAI SER TRUE
+let numero1 = 1
+let numero2 = 0
+let resultado = 1 || 0
+console.log(numero1, '||', numero2, '=', resultado)*/
+
+/*MAIOR >
+let numero1 = 3
+let numero2 = 5
+let resultado = numero1 > numero2
+console.log(numero1, '>', numero2, '=', resultado)*/
+
+/*MENOR <
+let numero1 = 4
+let numero2 = 6
+let resultado = numero1 > numero2
+console.log(numero1, '<', numero2, '=', resultado)*/
+
+/*MAIOR OU IGUAL >=
+let numero1 = 6
+let numero2 = 8
+let resultado = numero1 >= numero2
+console.log(numero1, '>=', numero2, '=', resultado)*/
+
+/*MENOR OU IGUAL <=
+let numero1 = 8
+let numero2 = 10
+let resultado = numero1 <= numero2
+console.log(numero1, '<=', numero2, '=', resultado)*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
